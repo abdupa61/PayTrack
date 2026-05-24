7f86b1b78e17b6a4795b11abf974a39ae524df19
Abdulsamet TÜTÜNCÜ <61270806+abdupa61@users.noreply.github.com>
2026-05-24 17:01:09 +0300

    Update get-debts.js

--- api/get-debts.js
+++ api/get-debts.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const url = process.env.KV_REST_API_URL;
   const token = process.env.KV_REST_API_TOKEN;
 
@@ -30,4 +30,4 @@ export default async function handler(req, res) {
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
-}
+};
